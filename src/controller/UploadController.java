0305eec8a3ae339c9444096942e7227f6de91eae
varun <bhattvarun29@gmail.com>
2015-12-11 15:21:49 +0530

    Added some email stuff, doesn't work yet

--- src/controller/UploadController.java
+++ src/controller/UploadController.java
@@ -1,5 +1,13 @@
 package controller;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -11,6 +19,7 @@ import javax.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * Created by fwarr on 02-Dec-15.
@@ -41,6 +50,86 @@ public class UploadController extends HttpServlet {
                 System.out.println(savePath + File.separator + fileName);
                 part.write(savePath + File.separator + fileName);
             }
+        }
+
+        System.out.println("Email sending");
+        /******************************************************************************************************/
+        // Recipient's email ID needs to be mentioned.
+        String to = "bhattvarun29@gmail.com";
+
+        // Sender's email ID needs to be mentioned
+        String from = "emandi74@gmail.com";
+
+        final String username = "emandi74";//change accordingly
+        final String password = "freshVeggies";//change accordingly
+
+        // Assuming you are sending email through relay.jangosmtp.net
+        String host = "smtp.gmail.com";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", "25");
+
+        // Get the Session object.
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+            System.out.println("inside try");
+            // Create a default MimeMessage object.
+            Message message = new MimeMessage(session);
+
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(from));
+
+            // Set To: header field of the header.
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+
+            // Set Subject: header field
+            message.setSubject("Testing Subject");
+
+            // Create the message part
+            BodyPart messageBodyPart = new MimeBodyPart();
+
+            // Now set the actual message
+            messageBodyPart.setText("This is message body");
+
+            // Create a multipar message
+            Multipart multipart = new MimeMultipart();
+
+            // Set text message part
+            multipart.addBodyPart(messageBodyPart);
+
+            System.out.println("Adding parts");
+            for(Part parts : request.getParts()){
+                multipart.addBodyPart((BodyPart)parts);
+            }
+            /*/ Part two is attachment
+            messageBodyPart = new MimeBodyPart();
+            String filename = "/home/manisha/file.txt";
+            DataSource source = new FileDataSource(filename);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(filename);
+            multipart.addBodyPart(messageBodyPart);*/
+
+            // Send the complete message parts
+            message.setContent(multipart);
+
+            // Send message
+            System.out.println("sending message");
+            Transport.send(message);
+
+            System.out.println("Sent message successfully....");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
         }
 
     }
